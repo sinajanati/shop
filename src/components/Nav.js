8d5613f14b253734cd72e67e79c5bbf6c35dbec5
sina janati <sinajanati@yahoo.com>
2023-05-23 10:30:25 +0330

    badge

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -17,7 +17,13 @@ const Nav = () => {
                             <NavLink to="/" className='nav-link'>home</NavLink>
                             </li>
                             <li className='nav-item'>
-                            <NavLink to="/cart" className='nav-link'>cart</NavLink>
+                            <NavLink to="/cart" className='nav-link'><button type="button" class="btn btn-primary position-relative">
+                  cart
+                  <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+                    {cart.length}
+                    <span class="visually-hidden">unread messages</span>
+                  </span>
+                </button></NavLink>
                             </li>
                     </ul>
                 </div>
